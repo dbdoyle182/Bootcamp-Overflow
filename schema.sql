9e491324be7e6711f5ceaa8808e20b01332e714e
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-06 17:36:12 -0400

    basic structure of app created

---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Erases database and recreates everytime this is run
+DROP DATABASE IF EXISTS boot_overflow;
+
+CREATE DATABASE boot_overflow;
---
+++ server.js
@@ -0,0 +1,40 @@
+// Dependencies
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Sets up the express app
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+// Requires our models for syncing db
+var db = require('./models');
+
+// Sets up the express app to handle data parsing
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Sets up the static directory
+
+app.use(express.static('public'));
+
+// Set up Handlebars
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import router
+
+// When there are controllers in place call them here
+
+app.use(routes);
+
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log('Server listening on: http://localhost:' + PORT)
+    });
+});
