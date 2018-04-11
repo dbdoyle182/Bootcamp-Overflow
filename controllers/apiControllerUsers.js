16112d306b7154774c834d687ed406668226b609
Juan Fox <foxj0875@gmail.com>
2018-04-11 10:31:17 -0400

    logout button functionality is coded, navbar is added to each page

--- controllers/apiControllerUsers.js
+++ controllers/apiControllerUsers.js
@@ -37,6 +37,12 @@ var db = require('../models');
         res.redirect('/user')
     });
 
+    router.get('/logout', function(req, res) {
+        req.logout();
+        req.session.destroy();
+        res.redirect('/');
+    })
+
 
 
 
